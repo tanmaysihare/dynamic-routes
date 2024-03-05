7fb8c2ed9011a61e55701e21587a9099780606c9
tanmaysihare <tanmaysihare@gmail.com>
2024-03-06 00:16:54 +0530

    cart

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get('/products/:productsId',shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart',shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);

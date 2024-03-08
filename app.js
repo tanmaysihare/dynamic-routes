9d431e514fe2d2de656ae03673b6ccd3c3aecdd2
tanmaysihare <tanmaysihare@gmail.com>
2024-03-08 18:39:32 +0530

    using sequelize now

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -25,4 +25,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result=> {
+   // console.log(result);
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+})
+
--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'sumansihare'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root','sumansihare',{
+    dialect:'mysql',
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
